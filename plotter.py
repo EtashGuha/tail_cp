28211f352d809491e9b5b8e98282092920c8f3da
Shlok Natarajan <shloknatarajan@salesforce.com>
2023-09-05 21:28:24 -0700

    feat: merged in cqr and added lauch config

--- plotter.py
+++ plotter.py
@@ -3,6 +3,7 @@ import os
 from cp import percentile_excluding_index
 import torch
 from cp import calc_length_coverage
+import pickle
 
 import seaborn as sns
 def set_style():
@@ -20,19 +21,27 @@ def set_style():
     })
 
 
-def plot_prob(args, range_vals, X_val, y_val, model):
+def plot_prob(args, range_vals, X_val, y_val, model, cqr_lower=None,cqr_upper=None):
     set_style()
     plt.rcParams["mathtext.fontset"] = "cm"
+    if not os.path.exists("images"):
+        os.mkdir("images")
     if not os.path.exists("images/{}".format(args.model_path)):
         os.mkdir("images/{}".format(args.model_path))
         os.mkdir("images/{}/right".format(args.model_path))
         os.mkdir("images/{}/wrong".format(args.model_path))
     step_val = (max(range_vals) - min(range_vals))/len(range_vals)
-    indices = (((y_val - min(range_vals)))/step_val).to(torch.int)
+    indices = ((y_val - min(range_vals))/step_val).to(torch.int)
     indices[indices == len(range_vals)] = indices[indices == len(range_vals)] - 1
     scores = torch.nn.functional.softmax(model(X_val), dim=1)
     all_scores = scores[torch.arange(len(X_val)), indices.long()]
-
+    # cqr_scores the same way and then plot both
+    ## Manually added cqr stuff
+    # cqr_scores = None
+    # with open('/Users/shloknatarajan/tail_cp/cqr_models/concrete-2023-09-02-17:31:34.pkl', 'rb') as f:
+    #     cqr_model = pickle.load(f)
+    #     cqr_scores = torch.nn.functional.softmax(cqr_model(X_val), dim=1)
+    #     cqr_scores = cqr_scores[torch.arange(len(X_val)), indices.long()]
 
     alpha = args.alpha
     for i in range(len(X_val)):
@@ -48,6 +57,10 @@ def plot_prob(args, range_vals, X_val, y_val, model):
 
         plt.plot([torch.min(range_vals).detach().numpy(), torch.max(range_vals).detach().numpy()], [percentile_val.detach().numpy(), percentile_val.detach().numpy()], label="cutoff")
         plt.plot([y_val[i].detach().numpy(), y_val[i].detach().numpy()], [torch.min(scores).detach().numpy(), torch.max(scores).detach().numpy()], label="truth")
+        if cqr_lower is not None:
+            plt.plot([cqr_lower[i], cqr_lower[i]], [torch.min(scores).detach().numpy(), torch.max(scores).detach().numpy()], label="cqr lower", color="red")
+        if cqr_upper is not None:
+            plt.plot([cqr_upper[i], cqr_upper[i]], [torch.min(scores).detach().numpy(), torch.max(scores).detach().numpy()], label="cqr upper", color="red")
         plt.legend()
         
         if coverage == 1:
