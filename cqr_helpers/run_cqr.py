1853008939b089acdb7fedffa5db5861808b164b
EtashGuha <etashguha@gmail.com>
2023-09-19 13:32:02 +0900

    added runcqr

--- cqr_helpers/run_cqr.py
+++ cqr_helpers/run_cqr.py
@@ -13,14 +13,14 @@ import pickle
 import copy
 
 def run_cqr(args):
-    if not args.cqr_no_clipping and os.path.exists("saved_results/{}/cqr.pkl".format(args.dataset_name)):
-            with open("saved_results/{}/cqr.pkl".format(args.dataset_name), "rb") as f:
-                coverages, lengths = pickle.load(f)
-            return np.mean(coverages), np.std(coverages), np.mean(lengths), np.std(lengths), np.std(coverages)/np.sqrt(len(coverages)), np.std(lengths)/np.sqrt(len(lengths))
-    elif os.path.exists("saved_results/{}/cqr_nc.pkl".format(args.dataset_name)):
-            with open("saved_results/{}/cqr_nc.pkl".format(args.dataset_name), "rb") as f:
-                coverages, lengths = pickle.load(f)
-            return np.mean(coverages), np.std(coverages), np.mean(lengths), np.std(lengths), np.std(coverages)/np.sqrt(len(coverages)), np.std(lengths)/np.sqrt(len(lengths))
+    if not args.cqr_no_clipping and os.path.exists("saved_results/{}/cqr.pkl".format(args.dataset_name)) and os.path.exists("saved_results/{}/cqr_predictions.pkl".format(args.dataset_name)):
+        with open("saved_results/{}/cqr.pkl".format(args.dataset_name), "rb") as f:
+            coverages, lengths = pickle.load(f)
+        return np.mean(coverages), np.std(coverages), np.mean(lengths), np.std(lengths), np.std(coverages)/np.sqrt(len(coverages)), np.std(lengths)/np.sqrt(len(lengths))
+    elif os.path.exists("saved_results/{}/cqr_nc.pkl".format(args.dataset_name)) and os.path.exists("saved_results/{}/cqr_predictions_nc.pkl".format(args.dataset_name)):
+        with open("saved_results/{}/cqr_nc.pkl".format(args.dataset_name), "rb") as f:
+            coverages, lengths = pickle.load(f)
+        return np.mean(coverages), np.std(coverages), np.mean(lengths), np.std(lengths), np.std(coverages)/np.sqrt(len(coverages)), np.std(lengths)/np.sqrt(len(lengths))
     input_size, range_vals = get_input_and_range(args)
     train_loader, val_loader = get_loaders(args)
 
