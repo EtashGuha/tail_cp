831e5def0d13b018724c923f96f5c3eef4110c78
EtashGuha <etashguha@gmail.com>
2023-09-19 16:17:52 +0900

    chr done

---
+++ chr/chr/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from chr.chr import models
+from chr.chr import coverage
+from chr.chr import black_boxes
+#from chr.chr import black_boxes_r
+from chr.chr import methods
+from chr.chr import others
+# from chr import others_r
